75a048512f1f2c9e86b43406dd8f84e61529db9b
Kasey Jones <joneskm72@gmail.com>
2020-09-21 22:28:21 -0400

    Added connection

---
+++ config/connection.js
@@ -0,0 +1,22 @@
+const mysql = require("mysql");
+const util = require("util");
+
+const connection = mysql.createConnection({
+  host: 'localhost',
+  port: 3306,
+  user: 'root',
+  password: 12345,
+  database: 'burgers_db'
+});
+
+connection.connect(err => {
+  if (err) {
+    console.error("error connecting " + err.stack);
+    return;
+  }
+   console.log("Connected as id " + connection.threadId);
+});
+
+connection.query = util.promisify(connection.query);
+
+module.exports = connection;
