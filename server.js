6d718f51bee2c022dfd64d8de39be4422077ba01
Kasey Jones <joneskm72@gmail.com>
2020-09-23 13:28:29 -0400

    added packages

--- server.js
+++ server.js
@@ -1,3 +1,19 @@
 const mysql = require("mysql");
 const db = require("./db/db");
 const express = require("express");
+const orm = require("./config/orm.js");
+const handlebars = require("express-handlebars");
+const routes = require("./controllers/burger_controller.js");
+
+var app = express();
+var PORT = process.env.PORT || 8080
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(express.static("public"));
+
+app.use(routes);
+
+app.listen(PORT, function() {
+  console.log("App listening on PORT " + PORT);
+});
