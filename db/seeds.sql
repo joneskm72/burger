f077598a3da234098673fa2cbc731110209745fb
Kasey Jones <joneskm72@gmail.com>
2020-09-23 13:27:52 -0400

    Added seeds

---
+++ db/seeds.sql
@@ -0,0 +1,5 @@
+INSERT INTO burgers(burger_name, devoured) VALUES ("Plain Burger", true);
+
+INSERT INTO burgers(burger_name, devoured) VALUES ("Cheese Burger", true);
+
+INSERT INTO burgers(burger_name, devoured) VALUES ("Veggie Burger", false);
